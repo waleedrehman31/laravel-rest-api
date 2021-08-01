4923b99b60fda2253d1d0440ca1da4bbb46edd1e
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-08-01 14:23:42 +0500

    Laravel REST API with Sanctum Authentication

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,4 +69,15 @@ class ProductController extends Controller
     {
         return Product::destroy($id);
     }
+
+    /**
+     * Search for name
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'.$name.'$')->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,12 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+//Route::resource('products', ProductController::class);
 
-//Route::get('/products', [ProductController::class, 'index']);
-//Route::post('/products', [ProductController::class, 'store']);
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
 
 
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
